1bd61e635bb19d47ab4de4ab9c1734af19ac03a2
admin <admin@admins-Mac-mini-2.local>
2017-10-17 17:18:12 -0700

    added supporty for infoPlist.strings

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var i18nStringsFiles = require('i18n-strings-files');
 var fs = require('fs');
 
 // Read 'Localizable.strings' and return it as an object containing the key/value pairs
-var data = i18nStringsFiles.readFileSync('Localizable.strings', 'UTF-8');
-console.log(data);
+var dataLocalizable = i18nStringsFiles.readFileSync('Localizable.strings', 'UTF-8');
+var dataInfoPlist = i18nStringsFiles.readFileSync('InfoPlist.strings', 'UTF-8');
+console.log(dataLocalizable, dataInfoPlist);
+
+
 
