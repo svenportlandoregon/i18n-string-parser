6a89d757dce32acec441eb22349f32b57acb7094
admin <admin@admins-Mac-mini-2.local>
2017-10-19 10:33:00 -0700

    refactored main.js function

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const jsonexport = require('jsonexport');
 const i18nStringsFiles = require('i18n-strings-files');
 const json2csv = require('json2csv');
-const papaparse = require('papaparse');
 const fs = require('fs');
 
 // Read 'Localizable.strings' and return it as an object containing the key/value pairs
-var dataLocalizable = i18nStringsFiles.readFileSync('Localizable.strings', 'UTF-8');
+var string2CSV = function() {
+
+var dataLocalizable = i18nStringsFiles.readFileSync('Localizable_de.strings', 'UTF-8');
 var dataInfoPlist = i18nStringsFiles.readFileSync('InfoPlist.strings', 'UTF-8');
 
 // var outputLocalizableCSV = JSON.stringify(dataLocalizable, null, 2);
@@ -14,17 +15,72 @@ var outputLocalizable = fs.writeFile("Localizable.json", JSON.stringify(dataLoca
     if (err) {
         return console.log(err);
     }
-    console.log("The file was saved!");
+    console.log("The JSON file was saved!");
 }); 
 
 jsonexport(dataLocalizable, function(err, csv) {
     if(err) {
-    return console.log(err);
+        return console.log(err);
     } else {
         fs.writeFile('Localizable.csv', csv, 'UTF-8');
-        console.log(csv);
+        console.log('The CSV file has been created');
     }
 });
+}
+
+string2CSV();
+
+module.exports = {
+    string2CSV: string2CSV
+};
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// // Directories to clean
+// var include = ['output/json/', 'output/txt/'];
+
+// // specific files to ignore
+// var exclude = ['.placeholder', '.DS_Store'];
+
+// // loops through each directory
+// for (var i = 0; i < include.length; i++) {
+
+//     // read files in directory
+//     var files = fs.readdirSync(include[i]);
+
+//     // loops through files in current directory
+//     for (var j = 0; j < files.length; j++) {
+//         var file = files[j];
+//         var del = true;
+
+//         // checks to see if current file is excluded
+//         for (var k = 0; k < exclude.length; k++) {
+//             if (file == exclude[k]) {
+//                 del = false;
+//             }
+//         }
+
+//         // deletes file if current file is not excluded
+//         if (del) {
+//             fs.unlink(include[i] + file, error => {
+//                 if (error) console.log('Error: ' + error.message);
+//                 else console.log('Removed ' + file);
+//             })
+//         }
+//     }
+// }
+
+
+
+
+
+
 
 //dictionary add labels to key:value pairs in object :: map, array
 // var readyForParsing = fs.readFileSync('Localizable.json', 'UTF-8');
